4cbd7c777148fd468c60f1eb79ab2897543b8e2f
Nikolay <nikolay.p.95.workmail@gmail.com>
2026-07-22 23:24:17 +0500

    Добавлено создание задач для анализа: - реализованы схемы: analyzer и task - реализованы репозитории для работы с бд: task_repository и result_repository - добавлен сервисный слой analyzer_service - подготовлены endpoints для создание задачи и получения результатов: analyzer и task

---
+++ app/api/v1/tasks.py
@@ -0,0 +1,58 @@
+from uuid import UUID
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.session import get_db
+from app.repositories.result_repository import WebsiteResultRepository
+from app.repositories.task_repository import TaskRepository
+from app.schemas.task import TaskResponse, WebsiteResultResponse
+
+router = APIRouter(prefix='/tasks', tags=['tasks'])
+
+@router.get('/{task_id}', response_model=TaskResponse)
+async def get_task(task_id: UUID, db: AsyncSession = Depends(get_db)) -> TaskResponse:
+    """
+    Получение информации о задаче анализа по UUID.
+    """
+    task_repository = TaskRepository(db)
+    task = await task_repository.get_task(task_id)
+
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='Задача не найдена')
+
+    return TaskResponse(task_id=task.id, status=task.status,
+                        total_urls=task.total_urls, processed_urls=task.processed_urls, error=task.error,
+                        created_at=task.created_at, updated_at=task.updated_at)
+
+
+@router.get('/{task_id}/results', response_model=list[WebsiteResultResponse])
+async def get_task_results(task_id: UUID, db: AsyncSession = Depends(get_db)) -> list[WebsiteResultResponse]:
+    """
+    Получение списка результатов анализа для конкретной задачи
+    """
+    task_repository = TaskRepository(db)
+    result_repository = WebsiteResultRepository(db)
+
+    task = await task_repository.get_task(task_id)
+
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail='Задача не найдена')
+
+    results = await result_repository.get_results(task_id)
+
+    return [WebsiteResultResponse(
+                id=result.id,
+                task_id=result.task_id,
+                url=result.url,
+                status_code=result.status_code,
+                response_time_ms=result.response_time_ms,
+                title=result.title,
+                description=result.description,
+                links_count=result.links_count,
+                images_count=result.images_count,
+                html_size_bytes=result.html_size_bytes,
+                cached=result.cached,
+                error=result.error,
+                created_at=result.created_at,
+            ) for result in results]
---
+++ app/repositories/task_repository.py
@@ -0,0 +1,31 @@
+from uuid import UUID
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.db.models import AnalysisTask, AnalysisTaskStatus
+
+
+class TaskRepository:
+    """
+    Репозиторий для работы с задачами анализа.
+    """
+    def __init__(self, db: AsyncSession):
+        self.db = db
+
+    async def create_task(self, total_urls: int) -> AnalysisTask:
+        """
+        Создание новой задачи.
+        !!! Commit будет выполняться на service уровне.
+        """
+        task = AnalysisTask(status=AnalysisTaskStatus.PENDING.value,
+                            total_urls=total_urls, processed_urls=0)
+
+        self.db.add(task)
+        await self.db.flush()
+
+        return task
+
+
+    async def get_task(self, task_id: UUID) -> AnalysisTask|None:
+        """
+        Получение задачи по UUID.
+        """
+        return await self.db.get(AnalysisTask, task_id)
